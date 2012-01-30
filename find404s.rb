f9f69a903f66406fed09af188b8cfae868880852
Amber Kayle Armstrong <amber@lexblog.com>
2012-01-30 07:47:16 -0800

    Added comments and text to Readme

--- find404s.rb
+++ find404s.rb
@@ -2,6 +2,13 @@
 require 'net/http'
 require 'uri'
 
+
+# Get response from uri_str. 
+# Follow up to 10 redirects until uri_str returns as
+# success or not success (basically not a redirect)
+# 
+# Returns true if not success or redirect
+# Returns false if success
 def fetch(uri_str, limit = 10)
   # You should choose better exception. 
   raise ArgumentError, 'HTTP redirect too deep' if limit == 0
@@ -10,30 +17,40 @@ def fetch(uri_str, limit = 10)
   
 
   response = Net::HTTP.get_response(url)
+  
+  # Check response
+  # Based off of http://ruby-doc.org/stdlib-1.8.7/libdoc/net/http/rdoc/Net/HTTPResponse.html
   case response
+  # Didn't 404 or redirect
   when Net::HTTPSuccess
     false
+  # Redirect
   when Net::HTTPRedirection
     puts "Following redirect..."
     new_url = URI.parse(response['location'])
     new_url_str = new_url.to_s
     
+    # Add domain if just path
     unless ( new_url_str.include? url.host )
       new_url_str.insert(0, url.host )
     end
     
+    # Add in http:// if needed
     unless ( new_url_str.match( /^http\:\/\// ) )
       new_url_str.insert( 0, "http://" )
     end
     
+    # Recursion!
     fetch( new_url_str, limit - 1 )
+    
+  # 404!
   else 
     true
   end
 end
 
 
-
+# Starts the checking of a url
 def is_broken?( url )
   puts "Checking #{url}"
   return fetch url
@@ -41,25 +58,32 @@ def is_broken?( url )
 end
 
 
+
+# Make sure have all args
 unless ARGV.length == 2
   puts "Dude, not the right number of arguments."
   puts "Usage: ruby filesplitter.rb ExportFile.txt\n"
   exit
 end
 
+# Assign args
 file_to_read = ARGV[0]
 file_to_write = ARGV[1]
 
+# Grab urls from file
 urls_wc = `wc -l #{file_to_read}`
 urls_total = urls_wc.split.first.to_i.to_i
 urls_to_check = IO.readlines( file_to_read )
+
+# Open file to write results to
 result = File.new(file_to_write,  "w+")
 
 
-
-
+# Counters
 urls_count = 0
 urls_broken = 0;
+
+# Check each url 
 urls_to_check.each do | url |
   url.chomp
   url.insert(0, "http://") unless(url.match(/^http\:\/\//))
